716db8a4b60d1db9cc092654906252f6e1313047
Khiem Ho Xuan <kkho@ubuntu.(none)>
2013-10-26 17:08:13 +0200

    new data

---
+++ code/src/com/emerald/main/Start.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.emerald.main;
+
+import com.emerald.startup.introscreen;
+
+/**
+ *
+ * @author Spartacus Rex
+ */
+public class Start extends introscreen{}
